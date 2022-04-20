3d378a4b5e723e0913ba72cd39c117a08ece9423
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-04-20 14:50:45 +0200

    Initial commit
    end description

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+/* 
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+Bonus (fatelo):
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
+
+const app = new Vue({
+    el: "#app",
+
+    data:{
+        message: 'Ciao Vue JS'
+    }
+})
